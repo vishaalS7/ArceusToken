5de13f300817222f72678ec1c162acfaa58c88f1
vishaalS7 <ivishaal1411@gmail.com>
2024-10-11 22:20:30 +0530

    updated nft image file

--- script/interactions.s.sol
+++ script/interactions.s.sol
@@ -6,7 +6,8 @@ import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 import {Arceus} from "../src/ArceusToken.sol";
 
 contract MintArceus is Script {
-    string public constant ARCEUS = "ipfs://QmTBFsx7YWsaPnH9QqVz9zvr5y1iY6zccQGGuseFbVXFSj/?filename=Arceus.json";
+    // string public constant ARCEUS = "ipfs://QmTBFsx7YWsaPnH9QqVz9zvr5y1iY6zccQGGuseFbVXFSj/?filename=Arceus.json";
+    string public constant ARCEUS = "https://ipfs.io/ipfs/QmS2WR7FpF1urtnBviX6MKmFV1vty1qDRstmCofKjSQ9NS/Arceus.png";
 
     function run() public {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("Arceus", block.chainid);
--- test/ArceusTest.t.sol
+++ test/ArceusTest.t.sol
@@ -9,7 +9,7 @@ contract arceusTest is Test {
     deployArceusScript public deploy;
     Arceus public arceus;
     address public USER = makeAddr("user");
-    string public constant ARCEUS = "ipfs://QmTBFsx7YWsaPnH9QqVz9zvr5y1iY6zccQGGuseFbVXFSj/?filename=Arceus.json";
+    string public constant ARCEUS = "https://ipfs.io/ipfs/QmS2WR7FpF1urtnBviX6MKmFV1vty1qDRstmCofKjSQ9NS/Arceus.png";
 
     function setUp() public {
         deploy = new deployArceusScript();
